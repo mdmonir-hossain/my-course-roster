82b5eb426f8dbc6055edd4e29628bf666e326f9c
mdmonir-hossain <136880861+mdmonir-hossain@users.noreply.github.com>
2023-09-15 01:00:41 +0600

    Toast added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Card from "./Components/Card/Card";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Header/Header";
 import { useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [carddata, setCarddata] = useState([]);
@@ -17,7 +19,7 @@ function App() {
 const handleCart=(course)=>{
   const isSelected = selectcourse.find((courselected) => courselected.id == course.id);
   if (isSelected) {
-    return alert('aready Select');
+    return toast("Already Selected");
   }
   setSelectcourse([...selectcourse,course])
 }
@@ -26,6 +28,7 @@ const handleCart=(course)=>{
     <>
       <div className="flex justify-center">
         <Header></Header>
+        <ToastContainer />
       </div>
       <div className="flex justify-between p-10">
         <Card carddata={carddata} handleCart={handleCart}></Card>
