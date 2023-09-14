01c9fe5d359d8ce333c367d55881f4188539d516
mdmonir-hossain <136880861+mdmonir-hossain@users.noreply.github.com>
2023-09-14 23:23:50 +0600

    cart section structure

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,22 +1,31 @@
+import { FiDollarSign } from "react-icons/fi";
+import { BsBook } from "react-icons/bs";
+
 const Card = ({ carddata }) => {
   console.log(carddata);
   return (
-    <div className="w-[75%] grid grid-cols-3 gap-4">
+    <div className="w-[75%] grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 gap-4">
       {carddata.map((Courseinfo) => (
         <div key={Courseinfo.id} className="cart">
-          <div className="card card-compact  bg-base-100 shadow-xl">
+          <div className="card card-compact  bg-base-100 shadow-xl p-2">
             <figure>
               <img src={Courseinfo.cover} alt="Cover" />
             </figure>
             <div className="card-body">
-              <h2 className="card-title">{Courseinfo.title}</h2>
+              <h2 className="text-[16px] font-bold">{Courseinfo.title}</h2>
               <p>{Courseinfo.details}</p>
             </div>
-            <div className="flex justify-between p-3">
-              <span>$ Price:{Courseinfo.price}</span>
-              <span> Credit:{Courseinfo.credit}</span>
+            <div className="flex justify-between">
+              <div className="flex items-center gap-2 p-2">
+                <FiDollarSign />
+                <span>Price:{Courseinfo.price}</span>
+              </div>
+              <div className="flex items-center gap-2 p-2">
+                <BsBook />
+                <span>Credit:{Courseinfo.credit}</span>
+              </div>
             </div>
-            <button className="btn btn-primary p-4">Select</button>
+            <button className="btn bg-[#2F80ED] text-white hover:text-black p-4">Select</button>
           </div>
         </div>
       ))}
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,11 +1,19 @@
-
-
 const Cart = () => {
-    return (
-        <div className="w-[20%]">
-             <h1>Cart</h1>
+  return (
+    <div className=" ml-4">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title text-[#2F80ED]">Credit Hour Remaining</h2>
+          <hr />
+          <h1 className="text-2xl font-bold">Course Name</h1>
+          <hr />
+          <h2 className="card-title text-gray-400">Total Credit Hour : </h2>
+          <hr />
+          <h2 className="card-title text-gray-500">Total Price : </h2>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
