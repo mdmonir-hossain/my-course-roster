088a0de14036ad9d585571ae621bde2b1b41d9f2
mdmonir-hossain <136880861+mdmonir-hossain@users.noreply.github.com>
2023-09-15 10:25:25 +0600

    Credit Hour Remaining,Total Credit Hour,Total Price Calculation

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,48 @@ import Card from "./Components/Card/Card";
 import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Header/Header";
 import { useEffect } from "react";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const [carddata, setCarddata] = useState([]);
   const [selectcourse, setSelectcourse] = useState([]);
-  
+  const [totalcreditcourse, setTotalcreditcourse] = useState(0);
+  const [reaminingcreditcourse, setReaminingcreditcourse] = useState(20);
+  const [totalprice, setTotalprice] = useState(0);
+
   useEffect(() => {
-    fetch('./course.json')
-      .then(res => res.json())
-    .then(data => setCarddata(data))
-  }, [])
-const handleCart=(course)=>{
-  const isSelected = selectcourse.find((courselected) => courselected.id == course.id);
-  if (isSelected) {
-    return toast("Already Selected");
-  }
-  setSelectcourse([...selectcourse,course])
-}
+    fetch("./course.json")
+      .then((res) => res.json())
+      .then((data) => setCarddata(data));
+  }, []);
+  const handleCart = (course) => {
+    const isSelected = selectcourse.find(
+      (courselected) => courselected.id == course.id
+    );
+    let totalcredit = course.credit;
+    let totalPriceCourse = course.price;
+    if (isSelected) {
+      return toast("Already Selected");
+    } else {
+    selectcourse.forEach((total) => {
+      totalcredit = totalcredit + total.credit;
+    })
+    selectcourse.forEach((totalPrice) => {
+      totalPriceCourse = totalPriceCourse + totalPrice.price;
+    })
+    }
+    
+    if (totalcredit > 20) {
+      return toast("Credit can not be greater than 20 hrs");
+    }
+    const reaminingcredit = 20 - totalcredit;
+
+    setTotalprice(totalPriceCourse)
+    setReaminingcreditcourse(reaminingcredit);
+    setTotalcreditcourse(totalcredit);
+    setSelectcourse([...selectcourse, course]);
+  };
 
   return (
     <>
@@ -32,7 +55,7 @@ const handleCart=(course)=>{
       </div>
       <div className="flex justify-between p-10">
         <Card carddata={carddata} handleCart={handleCart}></Card>
-        <Cart selectcourse={selectcourse} ></Cart>
+        <Cart selectcourse={selectcourse} totalcreditcourse={totalcreditcourse} reaminingcreditcourse={reaminingcreditcourse} totalprice={totalprice}></Cart>
       </div>
     </>
   );
--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,10 +1,11 @@
-const Cart = ({ selectcourse }) => {
+const Cart = ({ selectcourse,totalcreditcourse,reaminingcreditcourse,totalprice}) => {
   let count = 1;
+  
   return (
     <div className=" ml-4">
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
-          <h2 className="card-title text-[#2F80ED]">Credit Hour Remaining</h2>
+          <h2 className="card-title text-[#2F80ED]">Credit Hour Remaining: { reaminingcreditcourse } hrs</h2>
           <hr />
           <h1 className="text-2xl font-bold">Course Name</h1>
           {selectcourse.map((cartAdd) => (
@@ -15,9 +16,9 @@ const Cart = ({ selectcourse }) => {
             </div>
           ))}
           <hr />
-          <h2 className="card-title text-gray-400">Total Credit Hour : </h2>
+          <h2 className="card-title text-gray-400">Total Credit Hour :{totalcreditcourse} </h2>
           <hr />
-          <h2 className="card-title text-gray-500">Total Price : </h2>
+          <h2 className="card-title text-gray-500">Total Price :{totalprice} </h2>
         </div>
       </div>
     </div>
